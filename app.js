d59729185f0f0ded326b8982b7de30fe850ce10d
lstrichashl <lstrichashl@gmail.com>
2016-10-28 20:57:50 +0300

    error handler middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var viewer = require('./routes/viewer');
 var attacheUUID = require("./middlewares/attachUUID");
+var errorHandler = require("./middlewares/errorHandler");
 
 var app = express();
 
 app.use(attacheUUID);
 app.use('/view',viewer);
+app.use(errorHandler);
 
 app.listen(3000);
